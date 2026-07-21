b4fdb68d60f0df6d107b27915abdaa5bef502648
Roman Ivanov <romanivanov531@gmail.com>
2026-07-21 17:55:42 +0300

    add type annotation

--- src/category.py
+++ src/category.py
@@ -5,7 +5,7 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name:str, description:str, products=None):
         if products is None:
             products = []
         self.name = name
