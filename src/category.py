5dd3b1dc1a7973f006f86e8a9c3a7c733eb0f4af
Roman Ivanov <romanivanov531@gmail.com>
2026-08-17 21:37:35 +0300

    done

--- src/category.py
+++ src/category.py
@@ -2,14 +2,25 @@ class Category:
     name: str
     description: str
     products: list
+
     category_count = 0
     product_count = 0
+    __products = []
 
-    def __init__(self, name: str, description: str, products=None):
-        if products is None:
+    def __init__(self, name: str, description: str, products):
+        if len(products) < 1:
             products = []
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @classmethod
+    def add_product(cls, product):
+        cls.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        return [f'{product.name}, {product.price} руб. Остаток: {product.quantity}' for product in self.__products]
